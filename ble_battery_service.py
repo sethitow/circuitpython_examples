df3de25061a52c1891351bec514948da7efad0d5
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-07-27 17:11:14 -0700

    Support reconnecting

--- ble_battery_service.py
+++ ble_battery_service.py
@@ -12,7 +12,7 @@ voltage_monitor = analogio.AnalogIn(board.VOLTAGE_MONITOR)
 # https://www.bluetooth.com/specifications/gatt/characteristics/
 battery_level_chara = bleio.Characteristic(bleio.UUID(0x2919), read=True, notify=True)
 
-# Create a service for our battery level characteristic
+# Create a service for battery information
 # 0x180F is the service UUID for publishing characteristics related to the battery.
 # UUIDs for common services are defined by Bluetooth SIG.
 # https://www.bluetooth.com/specifications/gatt/services/
@@ -20,20 +20,21 @@ battery_service = bleio.Service(bleio.UUID(0x180F), [battery_level_chara])
 
 # Create a peripherial with our service and start it
 periph = bleio.Peripheral([battery_service])
-periph.start_advertising()
 
 while True:
-    # Do some math to turn the analog value into a percentage.
-    battery_voltage = (voltage_monitor.value * 3.3) / 65536 * 2
-    # We assume the battery has a voltage between 3.0 and 4.2 volts.
-    battery_percent = 250 * (battery_voltage - 3) / 3
+    # Start advertising when not connected.
+    periph.start_advertising()
+    #  Wait for a connection
+    while not periph.connected:
+        pass
 
-    # Only set the value of the characteristic if a client is connected.
-    # Setting the charateristic value without a client connected throws an OSError
-    if periph.connected:
+    while periph.connected:
+        # Do some math to turn the analog value into a percentage.
+        battery_voltage = (voltage_monitor.value * 3.3) / 65536 * 2
+        # We assume the battery has a voltage between 3.0 and 4.2 volts.
+        battery_percent = 250 * (battery_voltage - 3) / 3
+
         # Characteristic values must be Bytes. The Battery Level characteristic
-        # specification says the value should be an unsigned 8-bit integer, so 
+        # specification says the value should be an unsigned 8-bit integer, so
         # we encode the integer as a single byte.
         battery_level_chara.value = int(battery_percent).to_bytes(1, "big")
-
-    time.sleep(0.2)
